1d0d50cd072567540f04e2ce0ad00014cf2bfb4d
AnastasiyaMazura <mazura.anastasiya@gmail.com>
2018-09-12 18:13:25 +0300

    all tests

--- src/index.js
+++ src/index.js
@@ -1,31 +1,60 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor(name) {
+        this.name = name;
+        this.array = [];
+        this.comparator = function (a, b) {
+            return a > b ? 1 : -1;
+        };
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.array.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.array[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.array.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.array;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        var newarray = [];
+        indices.sort(function(a, b) {
+            return a > b ? 1 : -1;
+        });
+        for (let i = 0; i < indices.length; i++) {
+            newarray.push(this.array[indices[i]]);
+        }
+        newarray.sort(this.comparator);
+        for (let i = 0; i < indices.length; i++) {
+            this.array[indices[i]] = newarray[i];
+        }
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    setComparator(compareFunction) {
+        this.comparator = compareFunction;
+    }
 }
 
 module.exports = Sorter;
+
+const sorter = new Sorter();
+sorter.add(2);
+sorter.add(1);
+
+sorter.sort([0, 1]);
+console.log(sorter.toArray()) // [1, 2]
+
+sorter.add(3);
+
+const reverseCompareFunction = (left, right) => right - left;
+sorter.setComparator(reverseCompareFunction);
+
+sorter.sort([0, 1, 2]);
+console.log(sorter.toArray()); // [3, 2, 1]
